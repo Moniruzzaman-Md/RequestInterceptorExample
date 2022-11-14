9bbbccbe807bdd0c2b0d678c46aaebd503d5e920
MZ-DGG360\zaman <mzaman762@gmail.com>
2022-11-14 09:59:07 +0600

    Add project files.

---
+++ RequestInterceptorExample/Controllers/AbcController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RequestInterceptorExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AbcController : ControllerBase
+    {
+        [HttpGet("Hello")]
+        public IActionResult Get()
+        {
+            return BadRequest();
+        }
+    }
+}
---
+++ RequestInterceptorExample/RequestInterceptor/CustomRequestInterceptor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RequestInterceptorExample.RequestInterceptor
+{
+    public class CustomRequestInterceptor: ControllerBase
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomRequestInterceptor(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            throw new UnauthorizedAccessException("You are not Authorized");
+            await _next.Invoke(context);
+        }
+    }
+
+    public static class CustomMiddlewareExtensions
+    {
+        public static IApplicationBuilder CustomRequestInterceptor(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomRequestInterceptor>();
+        }
+    }
+}
